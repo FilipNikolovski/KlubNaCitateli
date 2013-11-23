0b71b86ffce194b80e8f3ef44a856f0023fb137b
Filip Nikolovski <fico_n@yahoo.com>
2013-11-23 19:35:15 +0100

    Database Klasa za konekcija

---
+++ KlubNaCitateli/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace KlubNaCitateli
+{
+    public class Database
+    {
+        public MySqlConnection connection;
+
+        public Database()
+        {
+            connection = new MySqlConnection("Database=111151_books;Data Source=mysql.students.finki.ukim.mk;User Id=111151;Password=0809992450006fnk!");
+            connection.Open();
+        }
+
+    }
+}
--- KlubNaCitateli/Site.Master.cs
+++ KlubNaCitateli/Site.Master.cs
@@ -11,7 +11,7 @@ namespace KlubNaCitateli
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
     }
 }
