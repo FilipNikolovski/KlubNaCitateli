580973c245888bd5ca72409f1043d902cc0bbc43
Filip Nikolovski <fico_n@yahoo.com>
2014-02-21 18:45:03 +0100

    Najnova verzija

--- KlubNaCitateli/Sites/index.aspx.cs
+++ KlubNaCitateli/Sites/index.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
 
 namespace KlubNaCitateli.Sites
 {
     public partial class index : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-
+                
         }
+
+       
+        
+
     }
 }
